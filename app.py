d40ddbf44e8f5af24c2ab84c5c7d97caa979daee
Andrew <andrewfribush@gmail.com>
2015-04-01 23:10:01 -0400

    Basic app.py in place

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+CSRF_ENABLED = True
+SECRET_KEY = 'caocaoshuodaocaocao'
+
+@app.route('/', methods=['GET'])
+def page():
+	return render_template('index.html')
+
+app.run(debug=True)
